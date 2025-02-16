0bbe1148838f4df0ed07b5bca8c170a46e8a05ef
sunflour <myasnoypelmesh@gmail.com>
2025-02-16 16:10:36 +0300

    qwe

--- DogHouse/Pages/AuthorizationPage.xaml.cs
+++ DogHouse/Pages/AuthorizationPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using DogHouse.DB;
+using DogHouse.PageList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,40 @@ namespace DogHouse.Pages
     /// </summary>
     public partial class AuthorizationPage : Page
     {
+        public static List<Employee> employees { get; set; }
+
         public AuthorizationPage()
         {
             InitializeComponent();
+        }
+
+       
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTb.Text.Trim();
+            string pass = PassTb.Text.Trim();
+
+            employees = new List<Employee>(App.db.Employee.ToList());
+            Employee currentEmpl = employees.FirstOrDefault(x => x.Login == login && x.Password == pass);
+            if (currentEmpl != null)
+            {
+                
+                NavigationService.Navigate(new SurveyPage());
+            }
+            else if (LoginTb.Text == "0000" && PassTb.Text == "0000")
+                NavigationService.Navigate(new AdminPage());
+            else
+            {
+                MessageBox.Show("Мы не нашли ваши данные в системе, попробуйте зайти снова");
+                LoginTb.Text = "";
+                PassTb.Text = "";
+            }
+        }
+
+        private void Guest_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
